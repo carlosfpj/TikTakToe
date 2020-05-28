a692a4ad97522ebf323430f85a41522a8d62646f
Carlos <cafer89@gmail.com>
2020-05-28 14:50:47 -0500

    data restored and displayed succesfully

--- app/src/main/java/com/cfpj/tiktaktoe/LoadGameActivity.java
+++ app/src/main/java/com/cfpj/tiktaktoe/LoadGameActivity.java
@@ -6,6 +6,8 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 public class LoadGameActivity extends AppCompatActivity {
@@ -45,27 +47,39 @@ public class LoadGameActivity extends AppCompatActivity {
 
             for(int i=0; i<cursorCount; i++) {
                 Log.d(LoadGameActivity.class.toString(), "DEFAULT CLASS VALUES = " + String.valueOf(id[i]) + " " + player1[i] + " " + player2[i] + " " + String.valueOf(score1[i])
-                                                                                  +  " " + String.valueOf(score2[i]) + " AT LINE 47");
+                                                                                  +  " " + String.valueOf(score2[i]) + " AT LINE 49");
             }
 
-             if (cursorCount > 0) {
-                while(cursor.moveToNext()) {
-                    for (int i = 0; i < cursorCount; i++) {
-                        id[i] = cursor.getInt(0);
-                        player1[i] = cursor.getString(1);
-                        player2[i] = cursor.getString(2);
-                        score1[i] = cursor.getInt(3);
-                        score2[i] = cursor.getInt(4);
-                        //test
-                        Log.d(LoadGameActivity.class.toString(), "CATCHING REAL CURSOR VALUES AT LINE 59, ID = " + id[i] + ", PLAYER 1 = " + player1[i] + ",  PLAYER 2 = " + player2[i] + ", SCORE 1 = " + score1[i] + ", SCORE 2 = " + score2[i]);
+             if (cursor != null && cursorCount !=0) {
+                if(cursor.moveToFirst()) {
+                    do{
+                        for (int i = 0; i < cursorCount; i++) {
+                            id[i] = cursor.getInt(0);
+                            player1[i] = cursor.getString(1);
+                            player2[i] = cursor.getString(2);
+                            score1[i] = cursor.getInt(3);
+                            score2[i] = cursor.getInt(4);
+                            cursor.moveToNext();
+                            //test, OK
+                            Log.d(LoadGameActivity.class.toString(), "CATCHING REAL CURSOR VALUES AT LINE 63, ID = " + id[i] + ", PLAYER 1 = " + player1[i] + ",  PLAYER 2 = " + player2[i] + ", SCORE 1 = " + score1[i] + ", SCORE 2 = " + score2[i]);
+                        }
                     }
+                    while (cursor.moveToNext());
                 }
+                 Log.d(LoadGameActivity.class.toString(), "CATCHING REAL CURSOR VALUES ID AT LINE 68, ID = " + id[0] + id[1]);
+                 LoadGameAdapter gameAdapter = new LoadGameAdapter(id,player1,player2,score1,score2);
+                 RecyclerView gameRecycler = findViewById(R.id.recView_games_loaded);
+                 gameRecycler.setAdapter(gameAdapter);
+                 RecyclerView.LayoutManager recyclerLayoutManager = new LinearLayoutManager(this);
+                 gameRecycler.setLayoutManager(recyclerLayoutManager);
+
             }else{
                  gamesNoFound();
             }
         } catch (SQLiteException e){
             Log.d(LoadGameActivity.class.toString(), "DATABASE UNAVAILABLE");
         }
+
     }
 
     public void gamesNoFound(){
---
+++ app/src/main/java/com/cfpj/tiktaktoe/LoadGameAdapter.java
@@ -0,0 +1,67 @@
+package com.cfpj.tiktaktoe;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class LoadGameAdapter extends RecyclerView.Adapter<LoadGameAdapter.ViewHolder> {
+
+    private int[] id, player1Score, player2Score;
+    private String[] player1Name, player2Name;
+
+
+    public LoadGameAdapter(int[] id, String[] player1Name, String[] player2Name, int[] player1Score, int[] player2Score){
+
+        this.id = id;
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+    }
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup viewGroup, int i) {
+
+        CardView v = (CardView) LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_loaded_game, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder( ViewHolder viewHolder, int i) {
+        CardView cardView = viewHolder.cardView;
+        TextView tvGameNumber = cardView.findViewById(R.id.tv_game_number);
+        TextView tvPlayer1Name = cardView.findViewById(R.id.tv_player1_name);
+        TextView tvPlayer1Score = cardView.findViewById(R.id.tv_player1_score);
+        TextView tvPlayer2Name = cardView.findViewById(R.id.tv_player2_name);
+        TextView tvPlayer2Score = cardView.findViewById(R.id.tv_player2_score);
+
+        tvGameNumber.setText(String.valueOf(id[i]));
+        tvPlayer1Name.setText(player1Name[i]);
+        tvPlayer1Score.setText(String.valueOf(player1Score[i]));
+        tvPlayer2Name.setText(player2Name[i]);
+        tvPlayer2Score.setText(String.valueOf(player2Score[i]));
+    }
+
+    @Override
+    public int getItemCount() {
+        return id.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private CardView cardView;
+
+        public ViewHolder(CardView v){
+            super(v);
+            cardView = v;
+
+        }
+
+    }
+
+}
